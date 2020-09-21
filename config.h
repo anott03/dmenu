75ebdfc05f576da495d47c71b1fb70edf466c6fb
Amitav Nott <amitav_nott@ryecountryday.org>
2020-09-21 18:55:17 -0400

    minor changes

--- config.h
+++ config.h
@@ -4,20 +4,22 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FiraCode Nerd Font:size=10"
+	"FiraCode Nerd Font:size=10",
+	"symbola:size=10",
+	"Symbola:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = "";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#292d3e" },
-	[SchemeSel] = { "#000000", "#e1acff" },
-	[SchemeSelHighlight] = { "#55007d", "#e1acff" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                               fg         bg       */
+	[SchemeNorm]             = { "#bbbbbb", "#292d3e" },
+	[SchemeSel]              = { "#000000", "#e1acff" },
+	[SchemeSelHighlight]     = { "#55007d", "#e1acff" },
+	[SchemeNormHighlight]    = { "#ffc978", "#222222" },
+	[SchemeOut]              = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 15;
+static unsigned int lineheight = 20;
 
 /*
  * Characters not considered part of a word while deleting words
